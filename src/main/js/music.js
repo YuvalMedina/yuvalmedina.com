12e77c6de8f6da7c8f6ed1d63236e3900657d144
YuvalMedina <yuval.medina@duke.edu>
2020-12-31 13:17:05 -0500

    dropdown for music videos is working

--- src/main/js/music.js
+++ src/main/js/music.js
@@ -1,18 +1,24 @@
-var buttons = document.getElementsByClassName("title");
+var buttons = document.getElementsByClassName('title');
 var i;
 
 for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var videoId = 'v' + this.id;
-    var content = document.getElementById(videoId);
-    var videoObjects = content.children;
-    videoObjects.forEach(element => {
-        if (element.style.maxHeight){
-            element.style.maxHeight = null;
+    buttons[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var videoId = 'v' + this.id.substring(1);
+        var content = document.getElementById(videoId);
+        var videoObjects = content.children;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
         } else {
-            element.style.maxHeight = 315 + "px";
+            content.style.maxHeight = 315 + "px";
+        }
+        for(var i = 0; i < videoObjects.length; i++){
+            var element = videoObjects[i];
+            if (element.style.maxHeight){
+                element.style.maxHeight = null;
+            } else {
+                element.style.maxHeight = 315 + "px";
+            }
         }
     });
-  });
 };
