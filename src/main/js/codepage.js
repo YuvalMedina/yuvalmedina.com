dd81fe7cc791143de56b03ebf4bb2c762181eb5b
YuvalMedina <yuval.medina@duke.edu>
2021-01-02 14:54:04 -0500

    Finalized intermediate version of codepage. Still need to figure out WebGL implementation of Shader, but not urgent or necessary.

--- src/main/js/codepage.js
+++ src/main/js/codepage.js
@@ -1,5 +1,10 @@
-var vertexUrl = "https://gist.githubusercontent.com/YuvalMedina/3c2557a9649316c323c147a4d806aadc/raw/d5b27cae9abefda87c0ef7d025978100f731a9c0/vertex.vert";
+let fragUrls = new Map();
+fragUrls['a1'] = 'https://gist.githubusercontent.com/YuvalMedina/947880787357fe8d372c0d17cf1b0dc7/raw/TetrationValueAnimation.frag';
+
+var vertexUrl = "https://gist.githubusercontent.com/YuvalMedina/3c2557a9649316c323c147a4d806aadc/raw/vertex.vert";
 var vertexData = null;
+
+var canvases = document.getElementsByTagName("canvas");
 
 var request = new XMLHttpRequest();
 
@@ -8,13 +13,17 @@ request.onreadystatechange = function() {
     if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
         var vertexData = request.responseText;
         console.log('-->' + 'vertex' + 'data:' + vertexData);
+        for(var i = 0; i < canvases.length; i++){
+            loadGlslAnimation(canvases[i]);
+        }
     }
 }
 request.send();
 
 var counter = 1;
 
-function loadGlslAnimation(element,url) {
+function loadGlslAnimation(element) {
+    var url = fragUrls[element.id];
     request.open("GET", url);
     request.onreadystatechange = function() {
         if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
@@ -31,6 +40,9 @@ function loadGlslAnimation(element,url) {
 }
 
 function startup(element, data){
+    var height = element.style.height;
+    var width = element.style.width;
+
     var glCanvas = element;
     var gl = glCanvas.getContext("webgl");
     var shaderProgram = gl.createProgram();
@@ -46,5 +58,9 @@ function startup(element, data){
     gl.attachShader(shaderProgram, fragShader);
     gl.attachShader(shaderProgram, vertexShader);
     gl.linkProgram(shaderProgram);
+
+    var u_resolutionLocation = gl.getUniformLocation(shaderProgram, "u_resolution");
+    gl.uniform2f(u_resolutionLocation, width, height);
+
     gl.useProgram(shaderProgram); 
 }
