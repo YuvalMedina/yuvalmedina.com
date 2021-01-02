5b977ffa138c4f4d5a3320e7621b33d31860f183
YuvalMedina <yuval.medina@duke.edu>
2021-01-02 13:34:09 -0500

    Added functionality in music page to automatically have some videos expanded at first.

--- src/main/js/music.js
+++ src/main/js/music.js
@@ -7,17 +7,29 @@ for (i = 0; i < buttons.length; i++) {
         var videoId = 'v' + this.id.substring(1);
         var content = document.getElementById(videoId);
         var videoObjects = content.children;
+        // videos that are displayed at first will have a different transition from expanded to collapsed.
+        var displayfirst = content.getAttribute("title") == "displayfirst";
         if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
-            content.style.maxHeight = 315 + "px";
+            if(displayfirst){
+                content.style.maxHeight = 0 + "px";
+            }
+            else{
+                content.style.maxHeight = 315 + "px";
+            }
         }
-        for(var i = 0; i < videoObjects.length; i++){
-            var element = videoObjects[i];
+        for(var j = 0; j < videoObjects.length; j++){
+            var element = videoObjects[j];
             if (element.style.maxHeight){
                 element.style.maxHeight = null;
             } else {
-                element.style.maxHeight = 315 + "px";
+                if(displayfirst){
+                    element.style.maxHeight = 0 + "px";
+                }
+                else{
+                    element.style.maxHeight = 315 + "px";
+                }
             }
         }
     });
