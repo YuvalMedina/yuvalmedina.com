bdc39a8fdebd9d2f78eb90e09d022ddeac0d72ed
YuvalMedina <yuval.medina@duke.edu>
2021-01-05 10:47:27 -0500

    floating objects on mainpage fixed. now need to figure out mobile version

--- var/www/js/mainpage.js
+++ var/www/js/mainpage.js
@@ -1,31 +1,88 @@
 var floatElmts;
 
-class floatObj{
-    constructor(floatElmt, x, y){
-        this.floatElmt = floatElmt;
+class delta{
+    constructor(x,y){
         this.x = x;
         this.y = y;
-        this.direction = Math.floor(Math.random()*8);
     }
+}
+
+var xDeltas = [1,1,1,0,-1,-1,-1,0];
+var yDeltas = [-1,0,1,1,1,0,-1,-1];
+
+class floatObj{
+    constructor(floatElmt,direction){
+        this.floatElmt = floatElmt;
+        this.direction = direction;
+    }
+}
+
+let deltas = [];
+
+for(var i = 0; i < xDeltas.length; i++){
+    var myDelta = new delta(xDeltas[i],yDeltas[i]);
+    deltas.push(myDelta);
 }
 
 var xMin=0;
 var xMax;
 var yMin;
 var yMax;
-var moveby = 1;
+var moveBy = 2;
+var drawing = false;
 
 let floatObjs = [];
 window.addEventListener('load', function(){
     floatElmts = document.getElementsByClassName("float");
     for(var i = 0; i < floatElmts.length; i++){
         var elmt = floatElmts[i];
-        floatObjs.push(new floatObj(elmt, elmt.offsetLeft, elmt.offsetTop));
+        var direction = randomNum(0,deltas.length);
+        var obj = new floatObj(elmt, direction);
+        floatObjs.push(obj);
     }
+    startup();
+    // redraw elements if window was resized
+    if(window.attachEvent) {
+        window.attachEvent('onresize', redraw);
+    }
+    else if(window.addEventListener) {
+        window.addEventListener('resize', redraw, true);
+    }
+    // stop elements from moving upon hover
+    for(var i = 0; i < floatElmts.length; i++){
+        var elmt = floatElmts[i];
+        elmt.addEventListener('mouseenter', stopmoving);
+        elmt.addEventListener('mouseleave', startmoving);
+    }
+});
+
+function stopdrawing(timeout){
+    drawing = false;
+    setTimeout('startmoving()', timeout);
+}
+
+function stopmoving(){
+    drawing = false;
+}
+
+function startmoving(){
+    drawing = true;
+    moveFloatObjs();
+}
+
+function startup(){
+    drawing = true;
     setBoundaries();
     positionTZero();
     moveFloatObjs();
-});
+}
+
+function redraw(){
+    stopdrawing(50);
+    setBoundaries();
+    positionTZero();
+    moveFloatObjs();
+}
 
 function setBoundaries(){
     var parent = floatElmts[0].parentElement;
@@ -35,98 +92,74 @@ function setBoundaries(){
     xMax = xMin + width - 100;
     yMin = absOffset.top;
     yMax = yMin + height - 100;
+    console.log("xMin: "+xMin+", xMax: "+xMax+", yMin: "+yMin+", yMax: "+yMax);
 }
 
 function positionTZero(){
     for(var i = 0; i < floatObjs.length; i++){
         var elmt = floatObjs[i].floatElmt;
-        var x = Math.random()*(xMax-xMin)+xMin;
-        var y = Math.random()*(yMax-yMin)+yMin;
+        var x = randomNum(xMin,xMax);
+        var y = randomNum(yMin,yMax);
         
         elmt.style.left = x+"px";
         elmt.style.top = y+"px";
-        floatObjs[i].x = x;
-        floatObjs[i].y = y;
     }
 }
 
 function moveFloatObjs(){
     for(var i = 0; i < floatObjs.length; i++){
         var elmt = floatObjs[i].floatElmt;
-        var x = floatObjs[i].x;
-        var y = floatObjs[i].y;
-        var direction = floatObjs[i].direction;
-        noise = Math.random();
-        var delta = getDelta(x, y, noise, direction);
+        var x = parseInt(elmt.style.left);
+        var y = parseInt(elmt.style.top);
+        var direction = getOrChangeDirection(x,y,floatObjs[i].direction);
+        var myDelta = deltas[direction];
 
-        var new_x = elmt.offsetLeft + delta.xDelta*moveby;
-        var new_y = elmt.offsetTop + delta.yDelta*moveby;
-        var new_dir = delta.direction;
+        var new_x = x + myDelta.x*moveBy;
+        var new_y = y + myDelta.y*moveBy;
 
-        floatObjs[i].x = new_x;
-        floatObjs[i].y = new_y;
-        floatObjs[i].direction = new_dir;
+        floatObjs[i].direction = direction;
         elmt.style.left = new_x+"px";
         elmt.style.top = new_y+"px";
     }
-    setTimeout('moveFloatObjs()',10);
+    if(drawing){
+        setTimeout('moveFloatObjs()',10);
+    }
 }
 
-function getDelta(x, y, noise, direction){
-    let xDeltas = [];
-    let yDeltas = [];
-    let change = noise<0.5? -1 : 1;
-    let choose = 0;
-    if(x <= xMin+moveby && y <= yMin+moveby){
-        xDeltas = [1,1,0];
-        yDeltas = [0,1,1];
-        choose = direction+change % 3;
+function getOrChangeDirection(x, y, direction){
+    var nowDelta = deltas[direction];
+    if(x+moveBy*nowDelta.x > xMin && x+moveBy*nowDelta.x < xMax
+        && y+moveBy*nowDelta.y > yMin && y+moveBy*nowDelta.y < yMax){
+        return direction;
     }
-    else if(x <= xMin+moveby && y >= yMax-moveby){
-        xDeltas = [0,1,1];
-        yDeltas = [-1,-1,0];
-        choose = direction+change % 3;
+    var chooseFrom = new Array(0,1,2,3,4,5,6,7);
+    if(x+moveBy*nowDelta.x <= xMin){
+        removeElements(chooseFrom, new Array(4,5,6));
+    } else if(x+moveBy*nowDelta.x >= xMax){
+        removeElements(chooseFrom, new Array(0,1,2));
+    } else if(y+moveBy*nowDelta.y <= yMin){
+        removeElements(chooseFrom, new Array(0,6,7));
+    } else if(y+moveBy*nowDelta.y >= yMax){
+        removeElements(chooseFrom, new Array(2,3,4));
     }
-    else if(x >= xMax-moveby && y <= yMin+moveby){
-        xDeltas = [-1,-1,0];
-        yDeltas = [0,1,1];
-        choose = direction+change % 3;
+    return chooseFrom[randomNum(0,chooseFrom.length)];
+}
+
+function removeElements(removeFrom, elements){
+    var i = 0;
+    while(i < removeFrom.length && elements.length > 0){
+        if(removeFrom[i] == elements[0]){
+            removeFrom.splice(i,1);
+            elements.splice(0,1);
+        } else {
+            i++;
+        }
     }
-    else if(x >= xMax-moveby && y >= yMax-moveby){
-        xDeltas = [-1,-1,0];
-        yDeltas = [0,-1,-1];
-        choose = direction+change % 3;
-    }
-    else if(x <= xMin+moveby){
-        xDeltas = [0,1,1,1,0];
-        yDeltas = [-1,-1,0,1,1];
-        choose = direction+change % 5;
-    }
-    else if(x >= xMax-moveby){
-        xDeltas = [0,-1,-1,-1,0];
-        yDeltas = [-1,-1,0,1,1];
-        choose = direction+change % 5;
-    }
-    else if(y <= yMin+moveby){
-        xDeltas = [-1,-1,0,1,1];
-        yDeltas = [0,1,1,1,0];
-        choose = direction+change % 5;
-    }
-    else if(y >= yMax-moveby){
-        xDeltas = [-1,-1,0,1,1];
-        yDeltas = [0,-1,-1,-1,0];
-        choose = direction+change % 5;
-    }
-    else{
-        xDeltas = [1,1,1,0,-1,-1,-1,0];
-        yDeltas = [-1,0,1,1,1,0,-1,-1];
-        choose = direction+change % 8;
-    }
-    return {
-        xDelta : xDeltas[choose],
-        yDelta : yDeltas[choose],
-        direction : choose
-    };
+}
+
+function randomNum(start,end){
+    var rand = Math.random();
+    return Math.floor(rand*(end-start)+start);
 }
 
 function cumulativeOffset(element) {
