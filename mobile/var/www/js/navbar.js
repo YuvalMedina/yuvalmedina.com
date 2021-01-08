f1ca0d4d78863ba62a3184764edc2479f99d1f9a
YuvalMedina <yuval.medina@duke.edu>
2021-01-08 12:48:25 -0500

    modifying navbar to automatically display if scrolling is not possible. modified navbar for mobile to stay sticky after just one scroll.

--- mobile/var/www/js/navbar.js
+++ mobile/var/www/js/navbar.js
@@ -1,13 +1,30 @@
-var scrollValue = 0;
+var scrolled = false;
 
-$(window).scroll(function(){
-    var x = $(this).scrollTop();
-    if(x > scrollValue) {
+// check if user can scroll through page:
+var hasVerticalScroll = document.body.scrollHeight > document.body.clientHeight;
+
+// Get the computed style of the body element
+var cStyle = document.body.currentStyle||window.getComputedStyle(document.body, "");
+
+// Check the overflow and overflowY properties for "auto" and "visible" values
+hasVerticalScroll = cStyle.overflow == "visible" 
+             || cStyle.overflowY == "visible"
+             || (hasVScroll && cStyle.overflow == "auto")
+             || (hasVScroll && cStyle.overflowY == "auto");
+
+$(document).ready(function() {
+    if (hasVerticalScroll) {
+        // can scroll vertically
+        $(window).scroll(function(){
+            if(!scrolled){
+                $(".my-navbar").addClass("sticky");
+                $(".my-nav-list").addClass("sticky");
+                scrolled = true;
+            }
+        });
+    } else {
+        // cannot scroll: automatically display navbar:
         $(".my-navbar").addClass("sticky");
         $(".my-nav-list").addClass("sticky");
-    } else {
-        $(".my-navbar").removeClass("sticky");
-        $(".my-nav-list").removeClass("sticky");
     }
-    scrollValue = x;
 });
