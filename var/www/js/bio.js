d8f044b752f97b3b338893fb541174a02ba03166
YuvalMedina <yuval.medina@duke.edu>
2021-07-03 17:20:20 -0400

    finalized bio section for website. submitting first draft for the internets.

---
+++ var/www/js/bio.js
@@ -0,0 +1,44 @@
+var bioUrl = "https://gist.githubusercontent.com/YuvalMedina/e8648ec80fdfe390659b855fdd258512/raw/yuvalmedinaBio.txt"
+var request = new XMLHttpRequest();
+
+// fills bio from text submitted on my git gist. link provided above
+
+request.open("GET", bioUrl);
+request.onreadystatechange = function() {
+    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+        var response = request.responseText;
+        console.log('-->' + 'bio' + 'data:' + response);
+        fillBio(response);
+    }
+}
+request.send();
+
+function fillBio(bioData){
+    var bioRow = document.createElement("div");
+    bioRow.className = "row cover";
+
+    var bioContainer = document.createElement("div");
+    bioContainer.className = "col-lg-6 biocolumn";
+
+    var bioSpan = document.createElement("span");
+    bioSpan.className = "biocontainer";
+    
+    bioText = document.createElement("p");
+    bioText.className = "bio";
+    bioText.innerHTML = bioData;
+
+    bioSpan.appendChild(bioText);
+    bioContainer.appendChild(bioSpan);
+
+    var imgContainer = document.createElement("div");
+    imgContainer.className = "col-lg-5";
+    img = document.createElement("img");
+    img.className = "yuvalportrait";
+    img.setAttribute("src", "https://i.imgur.com/x9em7h8.png");
+    imgContainer.appendChild(img);
+    
+    bioRow.appendChild(imgContainer);
+    bioRow.appendChild(bioContainer);
+
+    document.body.appendChild(bioRow);
+}
